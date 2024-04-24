9133f3e617b1ee551f4f3d83bbc29b54779ed762
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-04-24 23:29:32 +0530

    nav bar done

---
+++ script.js
@@ -0,0 +1,10 @@
+gsap.to("nav",{
+    backgroundColor:"rgb(19, 19, 19)",
+    scrollTrigger:{
+        scroller:".right",
+        trigger:"nav",
+        start:"top -15%",
+        end:"top -30%",
+        scrub:2,
+    }
+})
