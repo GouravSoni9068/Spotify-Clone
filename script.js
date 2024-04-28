9041f2b6f6cc75c7109f8580286c1dfc8b3f7429
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-04-29 02:03:33 +0530

    movement in seekBar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-gsap.to("nav",{
-    backgroundColor:"rgb(19, 19, 19)",
-    scrollTrigger:{
-        scroller:".right",
-        trigger:"nav",
-        start:"top -15%",
-        end:"top -30%",
-        scrub:2,
+gsap.to("nav", {
+    backgroundColor: "rgb(19, 19, 19)",
+    scrollTrigger: {
+        scroller: ".right",
+        trigger: "nav",
+        start: "top -15%",
+        end: "top -30%",
+        scrub: 2,
     }
 })
 
@@ -13,35 +13,34 @@ gsap.to("nav",{
 
 // Fetch all songs and return
 
-async function getSongs(){
-    let a=await fetch("http://127.0.0.1:3000/songs/")
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
 
-    let response=await a.text();
-    let div=document.createElement("div");
+    let response = await a.text();
+    let div = document.createElement("div");
 
-    div.innerHTML=response;
+    div.innerHTML = response;
     console.log(div);
 
-    let as=div.getElementsByTagName("a");
+    let as = div.getElementsByTagName("a");
     console.log(as);
 
-    let songs=[];
+    let songs = [];
     for (let i = 0; i < as.length; i++) {
         const element = as[i];
-        if(element.href.endsWith(".mp3"))
-        {
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href);
         }
     }
-    
+
     return songs;
 
 }
 
 // for play audio
 let audio = new Audio();
-function playAudio(track){
-    audio.src=track;
+function playAudio(track) {
+    audio.src = track;
     audio.play();
 }
 
@@ -49,26 +48,26 @@ function playAudio(track){
 function secondsToMinutes(seconds) {
     let minutes = Math.floor(seconds / 60);
     let remainingSeconds = Math.floor(seconds % 60);
-    
+
     // Add leading zero if seconds is less than 10
     let formattedSeconds = remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
     return minutes + ":" + formattedSeconds;
 }
 
-async function main(){
-    let SongCardContainer=document.querySelector(".song-cardContainer");
+async function main() {
+    let SongCardContainer = document.querySelector(".song-cardContainer");
 
-   // Get all songs
-    let all_songs=await getSongs();
+    // Get all songs
+    let all_songs = await getSongs();
     let addressOfSOng;
 
-  // Show all the songs in the playlist
+    // Show all the songs in the playlist
     all_songs.forEach(song => {
 
-        let songName=song.split("/songs/")[1];
-        addressOfSOng=song.split("/songs/")[0];
-        
-        SongCardContainer.innerHTML+=`
+        let songName = song.split("/songs/")[1];
+        addressOfSOng = song.split("/songs/")[0];
+
+        SongCardContainer.innerHTML += `
 
             <div id="pause" class="songCard">
 
@@ -86,75 +85,86 @@ async function main(){
                 </div>
             
             </div>`
-        });
+    });
 
 
     // PlAY SONGS
-        let music_player_PlayBtn =document.querySelector(".play");
-        console.log(music_player_PlayBtn)
-        let allSongsCard=SongCardContainer.querySelectorAll(".songCard")
+    let music_player_PlayBtn = document.querySelector(".play");
+    console.log(music_player_PlayBtn)
+    let allSongsCard = SongCardContainer.querySelectorAll(".songCard")
 
-        allSongsCard.forEach(sCard => {
+    allSongsCard.forEach(sCard => {
 
-            sCard.addEventListener("click",()=>{
+        sCard.addEventListener("click", () => {
 
-                let songName=sCard.querySelector(".songname").innerHTML;
-                let songPlay=`${addressOfSOng}/songs/${songName}`;
+            let songName = sCard.querySelector(".songname").innerHTML;
+            let songPlay = `${addressOfSOng}/songs/${songName}`;
 
-                playAudio(songPlay);  
+            playAudio(songPlay);
 
-                music_player_PlayBtn.innerHTML=`<i class="fa-solid fa-pause"></i>`
+            music_player_PlayBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`
 
-                let songInfo=document.querySelector(".songInfo");
-                songInfo.innerHTML=songName;
-                let songTime=document.querySelector(".songTime");
-                songTime.innerHTML="00:00/00:00";
-            })
-
-        });
-
-        music_player_PlayBtn.addEventListener("click",()=>{
-            if (audio.paused) {
-
-            // if 1st song play
-                if(audio.src=='')
-                {
-                    playAudio(all_songs[0]);
-
-                    let songInfo=document.querySelector(".songInfo");
-                    songInfo.innerHTML=allSongsCard[0].querySelector(".songname").innerHTML;
-
-
-                    let songTime=document.querySelector(".songTime");
-                    
-                }
-                else
-                {   
-                    audio.play();
-                }
-                music_player_PlayBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
-            }
-            else {
-                music_player_PlayBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
-                audio.pause();
-            }
+            let songInfo = document.querySelector(".songInfo");
+            songInfo.innerHTML = songName;
+            let songTime = document.querySelector(".songTime");
+            songTime.innerHTML = "00:00/00:00";
         })
 
-        
-    audio.addEventListener("timeupdate",()=>{
+    });
 
-        let songTime=document.querySelector(".songTime");
-        songTime.innerHTML=`${secondsToMinutes(audio.currentTime)}/${secondsToMinutes(audio.duration)}`;
+    // CLICK ON MUSIC PLAYER
 
-        let circle=document.querySelector(".circle")
-        circle.style.left=`${(audio.currentTime / audio.duration) * 100}`+"%"
-        if(circle.style.left=="100%")
-        {
+    music_player_PlayBtn.addEventListener("click", () => {
+        if (audio.paused) {
+
+            // if 1st song play
+            if (audio.src == '') {
+                playAudio(all_songs[0]);
+
+                let songInfo = document.querySelector(".songInfo");
+                songInfo.innerHTML = allSongsCard[0].querySelector(".songname").innerHTML;
+
+
+                let songTime = document.querySelector(".songTime");
+
+            }
+            else {
+                audio.play();
+            }
+            music_player_PlayBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+        }
+        else {
             music_player_PlayBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
             audio.pause();
-            circle.style.left="0%";
         }
+    })
+
+// UPDATE DURATION
+    audio.addEventListener("timeupdate", () => {
+
+        let songTime = document.querySelector(".songTime");
+        songTime.innerHTML = `${secondsToMinutes(audio.currentTime)}/${secondsToMinutes(audio.duration)}`;
+
+        circle.style.left = `${(audio.currentTime / audio.duration) * 100}` + "%"
+        if (circle.style.left == "100%") {
+            music_player_PlayBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
+            audio.pause();
+            circle.style.left = "0%";
+        }
+    })
+
+
+    // click on SEEKBAR 
+
+    let circle = document.querySelector(".circle")
+    let seekbar = document.querySelector(".seekbar");
+    seekbar.addEventListener("click", (e) => {
+        console.log('click');
+
+        console.log(e.target.getBoundingClientRect().width, e.offsetX);
+        circle.style.left=(e.offsetX/e.target.getBoundingClientRect().width)*100+'%';
         
+        audio.currentTime=(e.offsetX/e.target.getBoundingClientRect().width)*audio.duration;
     })
 
 }
